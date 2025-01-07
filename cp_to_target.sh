18483cb7d3ad802122db291dbe88bdd055ac260e
Maroje Marohnic <m.marohnic@framos.com>
2025-01-07 12:10:07 +0100

    remove duplicate

--- cp_to_target.sh
+++ cp_to_target.sh
@@ -11,22 +11,19 @@ TARGET_IP="$1"
 echo "Installing Framos modules to i.MX8MP board."
 
 DEVICE_TREE_DIR="/run/media/boot-mmcblk1p1"
-MODULES_DIR="/lib/modules/6.6.3-lts-next-gccf0a99701a7/updates"
+MODULES_DIR="/lib"
 OPT_BIN="/opt/imx8-isp"
 USR_LIB="/usr"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 
-echo "Copying device trees..."
+echo "Copying Image and device trees..."
+scp ./arch/arm64/boot/Image root@$TARGET_IP:/run/media/boot-mmcblk1p1
 scp ./linux-imx/arch/arm64/boot/dts/freescale/imx8mp-evk-imx*.dtb root@$TARGET_IP:$DEVICE_TREE_DIR
 
 echo "Copying vvcam sensor modules..."
-scp ./isp-vvcam/modules/vvcam-dwe.ko root@$TARGET_IP:$MODULES_DIR
-scp ./isp-vvcam/modules/vvcam-isp.ko root@$TARGET_IP:$MODULES_DIR
-scp ./isp-vvcam/modules/vvcam-video.ko root@$TARGET_IP:$MODULES_DIR
-scp ./isp-vvcam/modules/imx*.ko root@$TARGET_IP:$MODULES_DIR
-scp ./isp-vvcam/modules/max9679*.ko root@$TARGET_IP:$MODULES_DIR
+scp -r ./modules/lib/modules root@$TARGET_IP:$MODULES_DIR
 
 echo "Copying isp media server and isi drivers..."
 echo "Copying /opt/bin folder to ${OPT_BIN}"
